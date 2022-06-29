af634b7e29457a81137dcbc4b838447ff3c6097c
franksolis1998 <75344673+franksolis1998@users.noreply.github.com>
2022-06-28 21:17:00 -0500

    Add files via upload

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -1,7 +1,7 @@
-"""first_project URL Configuration
+"""ProTwo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from first_app import views
+from django.urls import path, include
+from appTwo import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.index,name='index')
+    path('', include('appTwo.urls')),
     path('admin/', admin.site.urls),
-    path('first_app/',include('first_app.urls'))
 ]
--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Models):
+    top_name = models.CharField(max_length=264,unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.FrorginKey(Topic)
+    name = models.CharField(max_length=264,unique=True)
+    path = models.URLField(unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DataField()
+
+    def __str__(self):
+        return str(self.date)
+
